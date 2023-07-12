489ea255140ed45f0f9271bbeccb5a27cb2eef3a
FrancoisSchoeman <francois.schoeman15@gmail.com>
2023-07-12 23:53:30 +0200

    added services and swagger

--- src/main/java/com/todoapi/todo_api/model/Todo.java
+++ src/main/java/com/todoapi/todo_api/model/Todo.java
@@ -1,18 +1,72 @@
 package com.todoapi.todo_api.model;
 
+import org.springframework.data.annotation.Id;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
 
-import jakarta.validation.constraints.NotBlank;
-
-public record Todo(
+@Entity
+public class Todo {
     @Id
-    Integer id,
+    private Integer id;
+
     @NotBlank
-    String task,
-    Boolean isComplete,
-    LocalDateTime dateCreated,
-    LocalDateTime dateUpdated
-) {
+    private String task;
+
+    private Boolean isComplete;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateUpdated;
+
+    public Todo() {}
+
+    public Todo(Integer id, String task, Boolean isComplete, LocalDateTime dateCreated, LocalDateTime dateUpdated) {
+        this.id = id;
+        this.task = task;
+        this.isComplete = isComplete;
+        this.dateCreated = dateCreated;
+        this.dateUpdated = dateUpdated;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public Boolean getIsComplete() {
+        return isComplete;
+    }
+
+    public void setIsComplete(Boolean isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(LocalDateTime dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
 }
